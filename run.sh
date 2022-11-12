f79cf2580e25dcfa9e3c87c97a5f4caa8c457a53
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2022-11-12 15:59:18 +0900

    wip

--- run.sh
+++ run.sh
@@ -3,10 +3,8 @@ set -xe
 docker run \
   --rm \
   --user 1000 \
-  --device /dev/ttyACM1 \
-  --device /dev/ttyACM0 \
   -e "DISPLAY=$DISPLAY" \
   -v "$HOME/.Xauthority:/$HOME/.Xauthority:ro" \
   --network host \
   -it \
-  pico-sigrok bash #pulseview
+  pico-sigrok pulseview
